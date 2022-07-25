9272541771bc9e9681573dd15e525d1473b52b10
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-07-25 12:48:19 +0100

    boostraping express application

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+dotenv.config();
+
+const app = express();
+
+// Secure HTTP Headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Enable CORS
+app.use(cors());
+
+// set view engines
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('views'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Import all routes
+
+// default route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to PostIt API v1' });
+});
+
+// Home route
+app.get('/api/v1/home', (req, res) => {
+  res.json({ message: 'Welcome to PostIt API v1' });
+});
+
+// Route Middleware
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,72 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+const chalk = require('chalk');
+
+// UNCAUGHT EXCEPTION
+// Application need to be crashed then a tool will be needed to restart the app
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(chalk.red(err.message));
+  console.log(chalk.red(err.stack));
+  process.exit(1);
+});
+
+const app = require('./app');
+
+// START SERVER
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (!Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port > 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || 1212);
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const server = app.listen(port, async () => {
+  const address = server.address();
+  // eslint-disable-next-line max-len
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${address.port}`;
+  console.log(`Listening on ${chalk.green(bind)}`);
+});
+
+// Catching Exception
+
+// Application does not necessarily need to be crashed
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(chalk.red(err.message));
+  console.log(chalk.red(err.stack));
+  process.exit(1);
+});
+
+process.on(
+  'SIGTERM',
+  () => {
+    console.log('👋 SIGTERM signal received.');
+    server.close(() => {
+      console.log('💥 Process terminated.');
+    });
+  } // end of SIGTERM
+);
+
+process.on(
+  'SIGINT',
+  () => {
+    console.log('👋 SIGINT signal received.');
+    server.close(() => {
+      console.log('💥 Process terminated.');
+    });
+  } // end of SIGINT
+);
